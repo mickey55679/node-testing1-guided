c4f31e63b4c1832d5e920fa673f0d9ddf115583c
Michaiah Bos <michaiahbos@yahoo.com>
2024-04-09 20:10:04 -0500

    wip

--- car/car.spec.js
+++ car/car.spec.js
@@ -1,1 +1,25 @@
 // Test away!
+function foo() {
+  return "the foo";
+}
+describe("our first tests", () => {
+  test("sanity", () => {
+    expect(5).toBe(5);
+  });
+  test("objects", () => {
+    const o = { a: 1 };
+    const oo = { a: 1 };
+    const ooo = oo;
+    expect(o).toBe(o);
+    expect(oo).toBe(ooo);
+  });
+  test("objects again", () => {
+    expect({ a: 1 }).toEqual({ a: 1 }); //same shape but not same object
+  });
+});
+describe("foo function", () => {
+  test("foo returns the foo", () => {
+    expect(foo()).toBe("the foo");
+    expect(foo()).toHaveLength(7)
+  });
+});
