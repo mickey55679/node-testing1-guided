ddc3d6abb5206891eaa0cac4dbdfe819ef2c09fa
Michaiah Bos <michaiahbos@yahoo.com>
2024-04-10 12:05:21 -0500

    wip

--- car/car.js
+++ car/car.js
@@ -1,1 +1,7 @@
 // Build a Car class!
+module.exports = class Car {
+  constructor(make, model) {
+    this.make = make
+    this.model = model
+  }
+};
--- car/car.spec.js
+++ car/car.spec.js
@@ -1,4 +1,5 @@
 // Test away!
+const Car = require("./car");
 function foo() {
   return "the foo";
 }
@@ -20,6 +21,19 @@ describe("our first tests", () => {
 describe("foo function", () => {
   test("foo returns the foo", () => {
     expect(foo()).toBe("the foo");
-    expect(foo()).toHaveLength(7)
+    expect(foo()).toHaveLength(7);
+  });
+  describe("Car class", () => {
+    test("it is defined", () => {
+      expect(Car).toBeDefined();
+      expect(Car).toBeInstanceOf(Function);
+    });
+    test("has model and make", () => {
+      const prius = new Car("toyota", "prius");
+      expect(prius).toHaveProperty("make");
+      expect(prius).toHaveProperty("model");
+      expect(prius.make).toBeDefined();
+      expect(prius).toEqual({make: 'toyota', model: 'prius'})
+    });
   });
 });
